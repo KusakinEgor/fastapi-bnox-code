2b1eaf38d9e1ced864de16c64dedffa6332cc088
Egor <bananaxvii812@gmail.com>
2025-12-19 19:50:35 +0500

    switch password hashing from bcrypt to argon2

--- backend/app/api/core/security.py
+++ backend/app/api/core/security.py
@@ -44,7 +44,7 @@ async def get_current_user(
     return user
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
